543a5f287702fe505e11aa7b2c343a5e9345c650
shubhambaidhya <shubhambaidhya7@gmail.com>
2024-12-03 21:13:48 +0545

    updated

--- src/Player.jsx
+++ src/Player.jsx
@@ -7,15 +7,18 @@ const Player = (props) => {
       }}
     >
       <img
+        src={props.image}
+        alt=""
         style={{
+          height: 600,
           width: "100%",
           border: "400px",
         }}
-        src={props.image}
-        alt=""
       />
 
       <h3 style={{ padding: "1rem" }}>{props.name}</h3>
+      <p style={{ padding: "1rem", textAlign: "justify" }}>{props.text}</p>
     </div>
   );
 };
+export default Player;
